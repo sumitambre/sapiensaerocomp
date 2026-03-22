95c1271fea6c25a4ee49b7aabcc0d23d2d52086d
Sumit Ambre <sumit.ambre1@GMAIL.COM>
2026-03-22 13:04:08 +0400

    Update images and footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,9 +3,6 @@
     <footer className="bg-navy-900 text-offwhite/70 py-10 px-4 text-sm">
       <div className="max-w-6xl mx-auto">
         <div className="flex flex-col md:flex-row md:items-center md:justify-between gap-6">
-          <p className="text-center md:text-left">
-            &copy; 2026 Sapiens AeroComp Pvt. Ltd. All rights reserved. Pune, India.
-          </p>
           <div className="flex flex-wrap justify-center md:justify-end gap-x-5 gap-y-2">
             <a className="hover:text-offwhite/90" href="/">
               Home
@@ -22,16 +19,12 @@
             <a className="hover:text-offwhite/90" href="/privacy">
               Privacy
             </a>
-            <a className="hover:text-offwhite/90" href="/sitemap.xml">
-              Sitemap
-            </a>
-            <a className="hover:text-offwhite/90" href="/robots.txt">
-              Robots
-            </a>
-            <a className="hover:text-offwhite/90" href="/llms.txt">
-              llms.txt
-            </a>
           </div>
+          <p className="text-center md:text-left">
+            &copy; 2026 Sapiens AeroComp Pvt. Ltd. All rights reserved. Pune, India.{" "}
+            <br/> Created by{" "}
+            <a href="mailto:official.creativedevs@gmail.com">Creativedevs</a>{" "}
+          </p>
         </div>
       </div>
     </footer>
